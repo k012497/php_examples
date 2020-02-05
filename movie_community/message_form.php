26274e37d33109ff86d319a1b920d531d73dd2cc
k012497 <k012497@gmail.com>
2020-02-05 21:45:49 +0900

    Update message_form.php

--- movie_community/message_form.php
+++ movie_community/message_form.php
@@ -10,30 +10,19 @@
             function check_input() {
                 if (!document.message_form.rv_id.value) {
                     alert("수신 아이디를 입력하세요!");
-                    document
-                        .message_form
-                        .rv_id
-                        .focus();
+                    document.message_form.rv_id.focus();
                     return;
                 } else if (!document.message_form.subject.value) {
                     alert("제목을 입력하세요!");
-                    document
-                        .message_form
-                        .subject
-                        .focus();
+                    document.message_form.subject.focus();
                     return;
                 } else if (!document.message_form.content.value) {
                     alert("내용을 입력하세요!");
-                    document
-                        .message_form
-                        .content
-                        .focus();
+                    document.message_form.content.focus();
                     return;
                 }
                 console.log("본애기");
-                document
-                    .message_form
-                    .submit();
+                document.message_form.submit();
             }
         </script>
     </head>
